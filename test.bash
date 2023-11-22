0fedccaa684f675be23a102feb2e02ca0e8e999a
Shuhey <shuhei.daigaku@gmail.com>
2023-11-22 09:33:03 +0900

    test

--- test.bash
+++ test.bash
@@ -2,24 +2,9 @@
 # SPDX-FileCopyrightText: 2023 Shuhei Yanagihori
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-	echo NG at Line $1
-	res=1
-}
 
-res=0
+out=$(seq 2 | ./plus)
+[ "${out}" = 3 ] || ng ${LINENO}
+[ "${out}" = 2 ] || ng ${LINENO}
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK
-exit $res
 
