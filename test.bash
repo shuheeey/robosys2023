c50200fa15c0ec0e81c064e3c56431b708dca372
Shuhey <shuhei.daigaku@gmail.com>
2023-11-22 11:30:11 +0900

    fix

--- test.bash
+++ test.bash
@@ -4,15 +4,18 @@
 
 ng () {
       echo NG at Line $1
-      res = 1
+      res=1
 }
 
 res=0
 
 out=$(seq 3 | ./plus)
 
-[ "${out}" = 6 ] || ${LINENO}
-#[ "${out}" = 6 ] || ${LINENO}
+IFS=$'\n' read -r -a lines <<< "$out"
+
+[ "${lines[0]}" = 6 ] || ng ${LINENO}
+[ "${lines[1]}" = 6 ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
