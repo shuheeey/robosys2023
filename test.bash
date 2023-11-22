fcf79096d661e854560c372e03f330c07f60189e
Shuhey <shuhei.daigaku@gmail.com>
2023-11-22 10:35:20 +0900

    test

--- test.bash
+++ test.bash
@@ -2,9 +2,19 @@
 # SPDX-FileCopyrightText: 2023 Shuhei Yanagihori
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+      echo NG at Line $1
+      res = 1
+}
+
+res = 0
 
 out=$(seq 3 | ./plus)
 
-[ "${out}" = 6 6 ]
+[ "${out}" = 6 ] || ${LINENO}
+[ "${out}" = 6 ] || ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
 
 
