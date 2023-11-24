142ea33184f0a5493c1343365444309eb06e8a80
Shuhey <shuhei.daigaku@gmail.com>
2023-11-24 14:38:38 +0900

    fix

--- test.bash
+++ test.bash
@@ -9,11 +9,15 @@ ng () {
 
 res=0
 
-out=$(seq 2 | ./plus)
+out=$(seq 5 | ./plus)
 
-IFS=$'\n' read -r -a lines <<< "$out"
+#IFS=$'\n' read -r -a lines <<< "$out"
 
-[ "${lines[0]}" = 3 ] || ng ${LINENO}
+#[ "${lines[0]}" = 3 ] || ng ${LINENO}
+
+[ "${out}" = "15
+120
+3.0"] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
