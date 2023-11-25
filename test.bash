6d0a39861bf3646539650863e49566580f659ecc
Shuhey <shuhei.daigaku@gmail.com>
2023-11-26 08:31:58 +0900

    add a command

--- test.bash
+++ test.bash
@@ -10,16 +10,19 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-
-#IFS=$'\n' read -r -a lines <<< "$out"
-
-#[ "${lines[0]}" = 3 ] || ng ${LINENO}
-
 [ "${out}" = "合計値:15
 乗算値:120
 平均値:3.0
 逆数和:137/60
 2のべき乗:32" ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]    || ng ${LINENO}
+[ "${out}" = "合計値:0
+乗算値:1
+平均値:0
+逆数和:0
+2のべき乗:1" ]             || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
