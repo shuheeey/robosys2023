f2651388068f7326f26ee12d1935fd63d16fabe6
Shuhey <shuhei.daigaku@gmail.com>
2023-11-26 21:22:17 +0900

    add a command

--- test.bash
+++ test.bash
@@ -16,6 +16,13 @@ out=$(seq 5 | ./plus)
 逆数和:137/60
 2のべき乗:32" ]    || ng ${LINENO}
 
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
